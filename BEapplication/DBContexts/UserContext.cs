40e289818c7054e01a9186fe69cbbd3eee1f803b
Victoria Nedelea <victorianedelea24@gmail.com>
2024-04-06 10:29:26 +0100

    StartUp

---
+++ BEapplication/DBContexts/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using BEapplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BEapplication.DBContexts
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; } = null;
+    }
+}
---
+++ BEapplication/Models/RequestNewUser.cs
@@ -0,0 +1,9 @@
+﻿namespace BEapplication.Models
+{
+    public class RequestNewUser
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
